e3ac9f023fb277663d23b7bc9f900b9624e0a3a7
gcastroid <57358057+gcastroid@users.noreply.github.com>
2020-10-24 17:10:32 -0300

    Update alu_tb.vhd

--- alu_tb.vhd
+++ alu_tb.vhd
@@ -7,39 +7,39 @@ end entity;
 architecture test of alu_tb is
    
    signal inputA: std_logic_vector(7 downto 0) := "11001100";
-	signal inputB: std_logic_vector(7 downto 0) := "00110011";
-	signal sub: std_logic;
-	signal oe: std_logic;
-	signal res: std_logic_vector(7 downto 0);
-	signal res_bus: std_logic_vector(7 downto 0);
-	signal cf: std_logic;
-	signal zf: std_logic;
+   signal inputB: std_logic_vector(7 downto 0) := "00110011";
+   signal sub: std_logic;
+   signal oe: std_logic;
+   signal res: std_logic_vector(7 downto 0);
+   signal res_bus: std_logic_vector(7 downto 0);
+   signal cf: std_logic;
+   signal zf: std_logic;
 	
-	constant t: time := 20 ns;
+   constant t: time := 20 ns;
 	
 begin
 
    dut: entity work.alu port map (inputA, inputB, sub, oe, res, res_bus, cf, zf);
 	
-	-- Control signals
-	process
-	begin
-	   sub <= '0';
-	   oe <= '0';
-	   wait for t;
-	   inputB <= "10110011";
-	   wait for t;
-	   inputA <= "10110011";
-	   wait for t;
-	   sub <= '1';
-	   wait for t;
-	   sub <= '0';
-	   inputB <= "00000000";
-	   wait for t;
-	   inputA <= "00001010";
-	   wait for t;
-	   oe <= '1';
-	   wait;
-	end process;
+   -- Control signals
+   process
+   begin
+      sub <= '0';
+      oe <= '0';
+      wait for t;
+      inputB <= "10110011";
+      wait for t;
+      inputA <= "10110011";
+      wait for t;
+      sub <= '1';
+      wait for t;
+      sub <= '0';
+      inputB <= "00000000";
+      wait for t;
+      inputA <= "00001010";
+      wait for t;
+      oe <= '1';
+      wait;
+   end process;
 	
 end test;
