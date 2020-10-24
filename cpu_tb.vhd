b101ff8d5cfcf098aa926801259a196da72a6582
gcastroid <57358057+gcastroid@users.noreply.github.com>
2020-10-24 17:23:30 -0300

    Update cpu_tb.vhd

--- cpu_tb.vhd
+++ cpu_tb.vhd
@@ -10,32 +10,32 @@ end entity;
 
 architecture test of cpu_tb is
    
-	signal rst: std_logic;
-	signal clk: std_logic;
-	signal output: std_logic_vector(7 downto 0);
+   signal rst: std_logic;
+   signal clk: std_logic;
+   signal output: std_logic_vector(7 downto 0);
 	
-	constant t: time := 20 ns;
+   constant t: time := 20 ns;
 	
 begin
 
    dut: entity work.cpu(structure) port map (rst, clk, output);
 	
-	-- Clock signal
-	process
-	begin
-	   clk <= '0';
-		wait for t/2;
-		clk <= '1';
-		wait for t/2;
-	end process;
+   -- Clock signal
+   process
+   begin
+      clk <= '0';
+      wait for t/2;
+      clk <= '1';
+      wait for t/2;
+   end process;
 
-	-- Control signals
-	process
-	begin
-	   rst <= '0';
-		wait for t;
-		rst <= '1';
-		wait;
-	end process;
+   -- Control signals
+   process
+   begin
+      rst <= '0';
+      wait for t;
+      rst <= '1';
+      wait;
+   end process;
 	
 end test;
