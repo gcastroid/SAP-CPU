ddb791778e790b1ab614db350005a07f7c2c75dc
gcastroid <57358057+gcastroid@users.noreply.github.com>
2021-03-10 14:29:34 -0300

    Update cpu.vhd

--- cpu.vhd
+++ cpu.vhd
@@ -3,13 +3,23 @@ use ieee.std_logic_1164.all;
 use work.components.all;
 
 entity cpu is
-   port(i_rst: in std_logic;
-	i_clk: in std_logic;
-	o_data: out std_logic_vector(7 downto 0));
+   port(
+   i_rst: in std_logic;
+   i_clk: in std_logic;
+   o_disp1: out std_logic_vector(7 downto 0);
+   o_disp2: out std_logic_vector(7 downto 0));
 end entity;
 
 architecture behave of cpu is
+	
+   -- Pin assignments
+   attribute chip_pin: string;
+   attribute chip_pin of i_rst: signal is "b8";
+   attribute chip_pin of i_clk: signal is "p11";
+   attribute chip_pin of o_disp2: signal is "a16, b17, a18, a17, b16, e18, d18, c18";
+   attribute chip_pin of o_disp1: signal is "d15, c17, d17, e16, c16, c15, e15, c14";
 
+   signal s_data: std_logic_vector(7 downto 0);
    signal s_bus: std_logic_vector(7 downto 0);
    signal s_alu: std_logic_vector(7 downto 0);
    signal r_regA: std_logic_vector(7 downto 0);
@@ -19,6 +29,7 @@ architecture behave of cpu is
    signal r_mem_addr: std_logic_vector(3 downto 0);
    signal s_instruction: std_logic_vector(3 downto 0);
    signal s_clk: std_logic;
+   signal s_clk_div: std_logic;
    signal s_zf: std_logic;
    signal s_cf: std_logic;
    signal r_zf: std_logic;
@@ -46,7 +57,8 @@ architecture behave of cpu is
 begin 
    
    -- Clock logic
-   s_clk <= (not s_hlt) and i_clk;
+   div: clk_div port map (i_rst, i_clk, s_clk_div);
+   s_clk <= (not s_hlt) and s_clk_div;
 
    -- Reg A
    RegA: reg generic map (8) port map (i_rst, s_clk, s_ai, s_ao, s_bus, r_regA, s_bus);
@@ -55,7 +67,7 @@ begin
    -- Instruction Reg
    RegI: instruction_reg port map (i_rst, s_clk, s_ii, s_io, s_bus, s_instruction, s_bus);
    -- Output Reg
-   RegO: reg generic map (8) port map (i_rst, s_clk, s_oi, '0', s_bus, o_data, open);
+   RegO: reg generic map (8) port map (i_rst, s_clk, s_oi, '0', s_bus, s_data, open);
    -- Memory Addr Reg
    RegM: reg generic map (4) port map (i_rst, s_clk, s_mi, '0', s_bus(3 downto 0), r_mem_addr, open);
    -- Flags Reg 
@@ -99,5 +111,9 @@ begin
    s_co <= s_control(2);
    s_jp <= s_control(1);
    s_fi <= s_control(0);
+
+   -- Displays
+   disp2: seven_seg_anode port map (s_data(7 downto 4), o_disp2);
+   disp1: seven_seg_anode port map (s_data(3 downto 0), o_disp1);
 	
 end behave;
