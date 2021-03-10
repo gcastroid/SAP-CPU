658489757d61d2a6dc26f44f22dc375880f670bf
gcastroid <57358057+gcastroid@users.noreply.github.com>
2021-03-10 14:34:35 -0300

    Update instruction_reg.vhd

--- instruction_reg.vhd
+++ instruction_reg.vhd
@@ -2,13 +2,14 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity instruction_reg is 
-   port(i_rst: in std_logic;
-	i_clk: in std_logic;
-	i_en: in std_logic;
-	i_oe: in std_logic;
-        i_data: in std_logic_vector(7 downto 0);
-        o_instruction: out std_logic_vector(3 downto 0);
-	o_data_bus: out std_logic_vector(7 downto 0));
+   port(
+   i_rst: in std_logic;
+   i_clk: in std_logic;
+   i_en: in std_logic;
+   i_oe: in std_logic;
+   i_data: in std_logic_vector(7 downto 0);
+   o_instruction: out std_logic_vector(3 downto 0);
+   o_data_bus: out std_logic_vector(7 downto 0));
 end entity;
 
 architecture behave of instruction_reg is 
