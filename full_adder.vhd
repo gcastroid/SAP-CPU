23f0d1cbb0fe365cb54e1cd8a97306cf2cb8924a
gcastroid <57358057+gcastroid@users.noreply.github.com>
2021-03-10 14:34:07 -0300

    Update full_adder.vhd

--- full_adder.vhd
+++ full_adder.vhd
@@ -2,11 +2,12 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity full_adder is 
-   port(i_a: in std_logic;
-	i_b: in std_logic;
-	i_c: in std_logic;
-	o_s: out std_logic;
-	o_c: out std_logic);
+   port(
+   i_a: in std_logic;
+   i_b: in std_logic;
+   i_c: in std_logic;
+   o_s: out std_logic;
+   o_c: out std_logic);
 end entity;
 
 architecture behave of full_adder is 
